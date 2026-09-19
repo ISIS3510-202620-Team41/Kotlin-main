d7d53e997b7b1bab19c30728c97fd78df73c6096
SebastianVO15 <s.vargaso2@uniandes.edu.co>
2026-09-18 22:44:39 -0500

    feat: add step 2 activity type selection view mockup (#4)

---
+++ app/src/main/java/com/group41/kotlinapp/Step2Activity.kt
@@ -0,0 +1,30 @@
+package com.group41.kotlinapp
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Step2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        WindowCompat.getInsetsController(window, window.decorView).apply {
+            isAppearanceLightStatusBars = true
+        }
+
+        setContentView(R.layout.activity_step2)
+
+        val root = findViewById<View>(R.id.main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
